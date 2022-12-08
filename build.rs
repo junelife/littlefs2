3d400ef0f771854818bd23bcc12093f786e02367
na <nathan.whittington@gmail.com>
2022-12-08 08:44:02 -0800

    Removed the unecessary build.rs

--- build.rs
+++
@@ -1,7 +0,0 @@
-// Copyright © June Life Inc. 2022. All rights reserved. No part of this file may be
-// distributed, reproduced, or used by entities other than June Life Inc. without express
-// written permission from June Life Inc.
-
-fn main() {
-    build_tools::configure_toolchain();
-}
